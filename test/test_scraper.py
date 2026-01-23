6b09dff71dbb906144e7d0582ff7b5c9a966e73a
Barnold88 <bandslover@outlook.com>
2026-01-23 02:15:41 +0000

    Add unit test file for scraper

---
+++ test/test_scraper.py
@@ -0,0 +1,14 @@
+import unittest
+from selenium import webdriver
+from src import scraper
+import time
+
+
+
+class TestScraper(unittest.TestCase):
+
+    def test_parse_site(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
