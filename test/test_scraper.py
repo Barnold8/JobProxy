9091c32abd703abe4262e8b81d8956b141776ca3
Barnold88 <bandslover@outlook.com>
2026-01-23 03:28:16 +0000

    Update relative path issue for init and add locales to queryparams

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,9 +1,7 @@
 import unittest
 from selenium import webdriver
 from src import scraper
-import time
-
-
+from src.job import JobType
 
 class TestScraper(unittest.TestCase):
 
@@ -12,19 +10,89 @@ class TestScraper(unittest.TestCase):
 
     def test_format_site_url(self):
 
-        tests = {
-            'Abidjan': None,
-            'Ecatepec': None,
-            'Ibadan': None, 
-            'MADRID': None,
-            'Qingdao': None,
-            'Ufa': None,
-            'Yangon': None
+        test_input = {
+            'Abidjan': scraper.QueryParams (
+                "Abidjan",
+                "cleaner",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "fr"
+            ),
+            'Ecatepec': scraper.QueryParams (
+                "Ecatepec",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "mx"
+            ),
+            'Ibadan': scraper.QueryParams (
+                "Ibadan",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "ng"
+            ), 
+            'MADRID': scraper.QueryParams (
+                "MADRID",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "es"
+            ),
+            'Qingdao': scraper.QueryParams (
+                "Qingdao",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "cn"
+
+            ),
+            'Ufa': scraper.QueryParams (
+                "Ufa",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "ru"
+            ),
+            'Yangon': scraper.QueryParams (
+                "Yangon",
+                "job_title",
+                1.0,
+                1 ,
+                10 ,
+                JobType.FULL_TIME,
+                "mm"
+            )
+
+        }
+        test_expected = {
+            'Abidjan': "",
+            'Ecatepec':  "",
+            'Ibadan': "",
+            'MADRID':  "",
+            'Qingdao': "",
+            'Ufa':  "",
+            'Yangon':  "",
 
         }
 
-        places = [,  ]
-        params = []
+        for key in test_input.keys():
+            self.assertEqual(
+                test_expected[key],
+                scraper.Scraper.format_site_url(test_input[key])
+            )
 
 
         assert False
