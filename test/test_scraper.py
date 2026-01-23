0e389de015f936eb07cdbe8048df7c9449f2a079
Barnold88 <bandslover@outlook.com>
2026-01-23 02:56:18 +0000

    Add cities

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -10,5 +10,24 @@ class TestScraper(unittest.TestCase):
     def test_parse_site(self):
         pass
 
+    def test_format_site_url(self):
+
+        tests = {
+            'Abidjan': None,
+            'Ecatepec': None,
+            'Ibadan': None, 
+            'MADRID': None,
+            'Qingdao': None,
+            'Ufa': None,
+            'Yangon': None
+
+        }
+
+        places = [,  ]
+        params = []
+
+
+        assert False
+
 if __name__ == "__main__":
     unittest.main()
