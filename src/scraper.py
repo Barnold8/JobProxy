f3fd3eb332307f06adef4bb779f8195ab568f809
Barnold88 <bandslover@outlook.com>
2026-01-23 01:44:46 +0000

    Add basic initialisation for driver

--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,23 @@
 from enum import Enum
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-class Driver: # Todo: add other drivers
-    FIREFOX = 0,
-    CHROME = 0
+class Driver(Enum):
+    FIREFOX = 1
+    CHROME = 2
 
 class Scraper:
 
     def __init__(self,driver_type:Driver):
-        pass
+        self.driver = self.init_driver(driver_type)
+
+    def init_driver(self,driver_type:Driver)-> None:
+        # Function not tested since its just variable assingment which is handled by external code (selenium)
+        match driver_type:
+            case Driver.FIREFOX:
+                self.driver = webdriver.Firefox()
+            case Driver.CHROME:
+                self.driver = webdriver.Chrome()
+        
+
