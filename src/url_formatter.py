02386a784eb5ece8be2d9ccde1e7edbeba81a190
Barnold88 <bandslover@outlook.com>
2026-01-24 03:32:19 +0000

    Implement type checking to ensure validity of incoming params

--- src/url_formatter.py
+++ src/url_formatter.py
@@ -1,5 +1,6 @@
 import scraper
-from job import JobSite
+from job import JobSite,QueryParams
+from dataclasses import is_dataclass
 
 class URL_Formatter:
 
@@ -16,7 +17,9 @@ class URL_Formatter:
                 )        
 
     def indeed(job_site:JobSite,params:scraper.QueryParams)-> str:
-        return f"{params.locale}.indeed.com/jobs?q={params.job_title}&l={params.location}&radius={params.radius}&from=searchOnDesktopSerp"
+        if isinstance(job_site,int) == False or isinstance(params, QueryParams) == False:
+            raise TypeError(f"\n\tjob_site: {type(job_site)}, EXPECTED: int\n\tparams: {type(params)}, EXPECTED: job.QueryParams \n\tJOB: {isinstance(job_site,int)}\n\tPARAMS: {isinstance(params, QueryParams)}")
+        return f"https://{params.locale}.indeed.com/jobs?q={params.job_title}&l={params.location}&radius={params.radius}&from=searchOnDesktopSerp"
 
     def reed(job_site:JobSite,params:scraper.QueryParams):
         pass
