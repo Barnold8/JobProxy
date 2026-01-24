10e35d7ac10475cebef878aef321d9bb3d0eb047
Barnold88 <bandslover@outlook.com>
2026-01-24 03:34:18 +0000

    Add newline

--- src/url_formatter.py
+++ src/url_formatter.py
@@ -19,6 +19,7 @@ class URL_Formatter:
     def indeed(job_site:JobSite,params:scraper.QueryParams)-> str:
         if isinstance(job_site,int) == False or isinstance(params, QueryParams) == False:
             raise TypeError(f"\n\tjob_site: {type(job_site)}, EXPECTED: int\n\tparams: {type(params)}, EXPECTED: job.QueryParams \n\tJOB: {isinstance(job_site,int)}\n\tPARAMS: {isinstance(params, QueryParams)}")
+        
         return f"https://{params.locale}.indeed.com/jobs?q={params.job_title}&l={params.location}&radius={params.radius}&from=searchOnDesktopSerp"
 
     def reed(job_site:JobSite,params:scraper.QueryParams):
