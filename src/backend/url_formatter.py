41bc0392faa99b997e3f9169d90677be5b5ef2b6
Barnold88 <bandslover@outlook.com>
2026-01-26 22:26:46 +0000

    Add helper functions and finish reed formatting

--- src/backend/url_formatter.py
+++ src/backend/url_formatter.py
@@ -1,6 +1,16 @@
+from typing import List
 from backend import scraper
-from .job import JobSite,JobType, QueryParams
+from .job import JobSite,JobType, QueryParams, Remote
 from dataclasses import is_dataclass
+
+def space_to_dash(string:str) ->str:
+    return string.replace(" ", "-")
+
+def args_have_value(args: List) -> bool:
+    for elem in args:
+        if elem != None:
+            return True
+    return False
 
 class URL_Formatter:
 
@@ -25,6 +35,8 @@ class URL_Formatter:
     def reed(job_site:JobSite,params:QueryParams)-> str:
          # https://www.reed.co.uk/jobs/{job-type}-{work-from-home (if remote)}-{job}-jobs-in-{location}?proximity={radius}&salaryFrom={aalarymin}&salaryTo={salarymax}  
         
+        arguments = [params.radius,params.salary_min,params.salary_max]
+
         url = "https://www.reed.co.uk/jobs/"
 
         match params.job_type:
@@ -39,6 +51,23 @@ class URL_Formatter:
                 url += "part-time-"
             case _:
                 url += ""
+
+        match params.remote:
+            case Remote.HYBRID:
+                url += "work-from-home-" # not sure how to handle this case yet since reed doesnt properly support hybrid options
+            case Remote.REMOTE:
+                url += "work-from-home-"
+            case _:
+                url += ""
+
+        url += space_to_dash(params.job_title) + "-" if len(params.job_title) > 0 else ""
+        url += "jobs-in-" + space_to_dash(params.location) if len(params.location) > 0 else ""
+        url += "?" if args_have_value(arguments) else ""
+
+        if params.radius != None:
+            url += f"proximity={params.radius}"
+        if params.salary_min != None and params.salary_max != None:
+            url += f"&salaryFrom={params.salary_min}&salaryTo={params.salary_max}"
 
         return url
 
