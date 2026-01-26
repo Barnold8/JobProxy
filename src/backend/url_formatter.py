e2a45a50489d6e109c957839a9842054be2df7a8
Barnold88 <bandslover@outlook.com>
2026-01-26 22:03:53 +0000

    Fix imports and usage for params

--- src/backend/url_formatter.py
+++ src/backend/url_formatter.py
@@ -1,5 +1,5 @@
 from backend import scraper
-from .job import JobSite,QueryParams
+from .job import JobSite,JobType, QueryParams
 from dataclasses import is_dataclass
 
 class URL_Formatter:
@@ -18,9 +18,28 @@ class URL_Formatter:
                 )        
 
     @staticmethod
-    def indeed(job_site:JobSite,params:scraper.QueryParams)-> str:
+    def indeed(job_site:JobSite,params:QueryParams)-> str:
         return f"https://{params.locale}.indeed.com/jobs?q={params.job_title}&l={params.location}&radius={params.radius}&from=searchOnDesktopSerp"
 
     @staticmethod
-    def reed(job_site:JobSite,params:scraper.QueryParams):
-        pass
+    def reed(job_site:JobSite,params:QueryParams)-> str:
+         # https://www.reed.co.uk/jobs/{job-type}-{work-from-home (if remote)}-{job}-jobs-in-{location}?proximity={radius}&salaryFrom={aalarymin}&salaryTo={salarymax}  
+        
+        url = "https://www.reed.co.uk/jobs/"
+
+        match params.job_type:
+
+            case JobType.GRADUATE:
+                url += "graduate-"
+            case JobType.FIXED_CONTRACT:
+                url += "contract-"
+            case JobType.FULL_TIME:
+                url += "full-time-"
+            case JobType.PART_TIME:
+                url += "part-time-"
+            case _:
+                url += ""
+
+        return url
+
+
