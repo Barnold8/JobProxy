265e4662ab26aca6ee6c4d7a7f0daf6bdb262e4e
Barnold88 <bandslover@outlook.com>
2026-01-31 22:59:57 +0000

    Cleanup

--- assets/helper_scripts/compile_configs.py
+++ assets/helper_scripts/compile_configs.py
@@ -4,8 +4,6 @@ BASE_DIR = 'assets/tests/test_configs'
 ABS_PATH = os.path.join(os.getcwd(),BASE_DIR)
 FILES    = [os.path.join(os.getcwd(),f"{BASE_DIR}/{f}") for f in os.listdir(ABS_PATH) if os.path.isfile(os.path.join(ABS_PATH, f))] # This is also our test_input
 
-
-
 for file_name in FILES:
     json_string = ""
     file_name_wo_path = file_name.split("/")[-1]
@@ -20,4 +18,3 @@ for file_name in FILES:
                 print(f"\"{file_name_wo_path}\" : {"{}"},")
             else:
                 print(f"\"{file_name_wo_path}\" : {contents},")
-        # print(f"\"Test{index}\" : {contents},")   
