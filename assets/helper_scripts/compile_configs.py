f0dc7e7af24dde56417ad32e487ca0db0b06d46e
Barnold88 <bandslover@outlook.com>
2026-01-31 22:57:15 +0000

    Add helper script that compiles test configs into pastable test cases

---
+++ assets/helper_scripts/compile_configs.py
@@ -0,0 +1,22 @@
+import os
+
+BASE_DIR = 'assets/tests/test_configs'
+ABS_PATH = os.path.join(os.getcwd(),BASE_DIR)
+FILES    = [os.path.join(os.getcwd(),f"{BASE_DIR}/{f}") for f in os.listdir(ABS_PATH) if os.path.isfile(os.path.join(ABS_PATH, f))] # This is also our test_input
+
+
+
+for file_name in FILES:
+    json_string = ""
+    file_name_wo_path = file_name.split("/")[-1]
+    
+    if file_name.split(".")[-1].lower() == "json":
+        
+        with open(file_name,"r") as file:
+            contents = file.read()
+            contents = contents.replace("true","True") # Just to match python syntax
+            if len(contents) == 0 :
+                print(f"\"{file_name_wo_path} : {"{}"}")
+            else:
+                print(f"\"{file_name_wo_path}\" : {contents},")
+        # print(f"\"Test{index}\" : {contents},")   
