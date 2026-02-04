b1b4ebfac49a6e6edb3c99415eb0f9a48c182529
Barnold88 <bandslover@outlook.com>
2026-02-04 22:08:23 +0000

    Update instance strings

--- src/backend/job.py
+++ src/backend/job.py
@@ -29,6 +29,7 @@ class Job:
     job_location     : str
     job_poster       : str
     job_posted_on    : str
+    job_href         : str
 
 @dataclass
 class JobSiteDetails:
@@ -39,6 +40,7 @@ class JobSiteDetails:
     job_location_id  : str
     job_poster_id    : str
     job_posted_on_id : str
+    job_href_id      : str
 
 @dataclass
 class QueryParams:
@@ -55,11 +57,16 @@ class QueryParams:
 # Need a method to load these on boot and not hardcode values, allows user to change IDs/ClassNames/CSS_Selector when needed and doesnt need to change codebase
 REED_INSTANCE = JobSiteDetails(
 
-    job_card_id      = ".index-module_jobCard__body__vWzBf",
+    job_card_id      = ".index-module_jobCard__body__vWzBf", # This is valid
     job_title_id     = ".index-module_jobResultHeading__title__r7Yqg",
-    job_salary_id    = "[data-qa=\"job-metadata-salary]\"",
-    job_location_id  = ".index-module_jobCard__body__vWzBf",
-    job_poster_id    = ".index-module_jobCard__body__vWzBf",
-    job_posted_on_id = ".index-module_jobCard__body__vWzBf",
+    job_salary_id    = "[data-qa='job-metadata-salary']", # This is valid but selenium says its wrong?
+    job_location_id  = "[data-qa='job-metadata-location']",
+    job_poster_id    = "[data-qa='job-metadata-salary]'",
+    job_posted_on_id = "[data-qa='job-metadata-salary]'",
+    job_href_id      = "https://www.github.com"
+
+
+    #data-qa="job-metadata-salary"
+    #data-qa='job-metadata-salary'
 
 )
